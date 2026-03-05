fe7630752479d61419b19838cfc306aa1d74d48c
kal <emilianokaleb@gmail.com>
2026-03-05 09:24:18 -0600

    feat(): nueva api para procesos estocasticos

---
+++ api-stochastic/app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+app = FastAPI(
+    name = "procesos_estocasticos"
+    description = "Una api con algunos algoritmos para facilitar el analisis de cadenas de markov"
+    version = "0.0.0"
+)
